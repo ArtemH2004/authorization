591f29f4e8277aaea4d9467c20bc8394ff09d419
ArtemH2004 <159913643+ArtemH2004@users.noreply.github.com>
2025-07-24 11:11:20 +0300

    folder-structure

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,21 @@
+import "@/app/(auth)/auth.styles.scss";
+import { DefaultLink } from "@/common/components/a/DefaultLink";
+import { DefaultInput } from "@/common/components/input/DefaultInput";
+
+export default function Login() {
+  return (
+    <section className="auth__section">
+      <h1>Логин</h1>
+
+      <form className="auth__form">
+        <DefaultInput />
+      </form>
+
+      <span className="auth__span">
+        {`Нет аккаунта? `}
+        {` `}
+        <DefaultLink title="Регистрация" linkTo="/register" />
+      </span>
+    </section>
+  );
+}
